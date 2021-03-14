a58dd16093cb6c6ff5f9b89e2a9e6b9a93401824
cabejackson <jacksoncaleb.calebjackson@gmail.com>
2021-03-14 00:48:39 -0500

    Completed initial set-up of 30min countdown timer

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,88 +1,42 @@
 import React, { useState, useRef, useEffect } from 'react';
+import Timer from 'react-compound-timer';
 import './App.css';
 
 const App = () => {
-  const [timerDays, setTimerDays] = useState('00');
-  const [timerHours, setTimerHours] = useState('00');
-  const [timerMinutes, setTimerMinutes] = useState('00');
-  const [timerSeconds, setTimerSeconds] = useState('00');
-
-  let interval = useRef();
-
-  const startTimer = () => {
-    const countDownDate = new Date('February 15, 2021 00:10:00').getTime();
-
-    interval = setInterval(() => {
-      const now = new Date().getTime();
-      const distance = countDownDate - now;
-
-      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-      const hours = Math.floor((distance % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)));
-      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-
-      if (distance < 0) {
-        //stop timer
-        clearInterval(interval.current)
-
-      }
-      else {
-        //update timer
-        setTimerDays(days);
-        setTimerHours(hours);
-        setTimerMinutes(minutes);
-        setTimerSeconds(seconds);
-      }
-    }, 1000)
-  }
-
-
-  //componentDidMount
-  useEffect(() => {
-    startTimer();
-    return () => {
-      clearInterval(interval.current)
-
-    };
-  })
-
   return (
-    // <h1>Countdown Timer</h1>
-    <section className="timer-container">
-      <section className="timer">
-        <div>
-          <span className="mdi mdi-calendar-clock timer-icon"></span>
-          <h2>Countdown Timer</h2>
-          <p>Countdown to... some time determined by some person</p>
-        </div>
-        <div>
-          <section>
-            <p>{timerDays}</p>
-            <p><small>Days</small></p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerHours}</p>
-            <p><small>Hours</small></p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerMinutes}</p>
-            <p><small>Minutes</small></p>
-          </section>
-          <span>:</span>
-          <section>
-            <p>{timerSeconds}</p>
-            <p><small>Seconds</small></p>
-          </section>
-        </div>
+    <Timer
+      initialTime={1800000}
+      startImmediately={false}
+      // onStart={() => console.log('onStart hook')}
+      // onResume={() => console.log('onResume hook')}
+      // onPause={() => console.log('onPause hook')}
+      // onStop={() => console.log('onStop hook')}
+      // onReset={() => console.log('onReset hook')}
+      direction="backward"
+    >
+      {({ start, resume, pause, stop, reset, getTimerState, getTime }) => (
+        <>
+          <div>
+            {/* <Timer.Days /> days
+                <Timer.Hours /> hours */}
+            <Timer.Minutes /> minutes
+                <Timer.Seconds /> seconds
+                {/* <Timer.Milliseconds /> milliseconds */}
+          </div>
 
-      </section>
-    </section>
-
-  );
+          <div>{getTimerState()} </div>
+          {/* <div>{getTime()}</div> */}
+          <br />
+          <div>
+            <button onClick={start}>Start</button>
+            <button onClick={pause}>Pause</button>
+            <button onClick={resume}>Resume</button>
+            <button onClick={stop}>Stop</button>
+            <button onClick={reset}>Reset</button>
+          </div>
+        </>
+      )}
+    </Timer>
+  )
 };
-
-
 export default App;
