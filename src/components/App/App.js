2bd2c57746ec05e524890a7333404c8a768a8536
cabejackson <jacksoncaleb.calebjackson@gmail.com>
2021-03-24 13:35:29 -0400

    updated hover btn for timer + centered time/text

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,39 +4,49 @@ import './App.css';
 
 const App = () => {
   return (
-    <Timer
-      initialTime={1800000}
-      startImmediately={false}
-      // onStart={() => console.log('onStart hook')}
-      // onResume={() => console.log('onResume hook')}
-      // onPause={() => console.log('onPause hook')}
-      // onStop={() => console.log('onStop hook')}
-      // onReset={() => console.log('onReset hook')}
-      direction="backward"
-    >
-      {({ start, resume, pause, stop, reset, getTimerState, getTime }) => (
-        <>
-          <div>
-            {/* <Timer.Days /> days
-                <Timer.Hours /> hours */}
-            <Timer.Minutes /> minutes
-                <Timer.Seconds /> seconds
-                {/* <Timer.Milliseconds /> milliseconds */}
-          </div>
+    <div className="twinkling">
+      <div className="stars">
+        <div className="clouds">
+          <div className="timer-container">
+            <Timer className="timer"
+              initialTime={1800000}
+              startImmediately={false}
+              // onStart={() => console.log('onStart hook')}
+              // onResume={() => console.log('onResume hook')}
+              // onPause={() => console.log('onPause hook')}
+              // onStop={() => console.log('onStop hook')}
+              // onReset={() => console.log('onReset hook')}
+              direction="backward"
+            >
+              {({ start, resume, pause, stop, reset, getTimerState, getTime }) => (
+                <>
+                  <div >
+                    {/* <Timer.Days /> days <Timer.Hours /> hours */}
+                    <div className="time-text">
+                      <Timer.Minutes /> minutes {" "}
 
-          <div>{getTimerState()} </div>
-          {/* <div>{getTime()}</div> */}
-          <br />
-          <div>
-            <button onClick={start}>Start</button>
-            <button onClick={pause}>Pause</button>
-            <button onClick={resume}>Resume</button>
-            <button onClick={stop}>Stop</button>
-            <button onClick={reset}>Reset</button>
+                      <Timer.Seconds /> seconds
+                    </div>
+                    {/* <Timer.Milliseconds /> milliseconds */}
+                  </div>
+
+                  {/* <div className="state-text">{getTimerState()} </div> */}
+                  {/* <div>{getTime()}</div> */}
+                  <br />
+                  <div>
+                    <button className="btn" onClick={start}>Start</button>
+                    <button className="btn" onClick={pause}>Pause</button>
+                    <button className="btn" onClick={resume}>Resume</button>
+                    <button className="btn" onClick={stop}>Stop</button>
+                    <button className="btn" onClick={reset}>Reset</button>
+                  </div>
+                </>
+              )}
+            </Timer>
           </div>
-        </>
-      )}
-    </Timer>
+        </div>
+      </div>
+    </div>
   )
 };
 export default App;
