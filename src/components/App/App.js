e85fbfaa899994577e43142ce259e0df438baf44
cabejackson <jacksoncaleb.calebjackson@gmail.com>
2021-05-05 09:27:52 -0400

    example with preferred names displayed

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,6 +3,12 @@ import Timer from 'react-compound-timer';
 import './App.css';
 
 const App = () => {
+  let fakePerson = {
+    username: "bowtiesandbooks",
+    legal_name: "Jesse",
+    preferred_name: "Jessica"
+    // name: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+  };
   return (
     <div className="twinkling">
       <div className="stars">
@@ -18,11 +24,19 @@ const App = () => {
               // onReset={() => console.log('onReset hook')}
               direction="backward"
             >
+
               {({ start, resume, pause, stop, reset, getTimerState, getTime }) => (
                 <>
                   <div >
+
+                    <div className="welcome-message">
+                      <h1>Welcome {fakePerson.preferred_name}!!</h1>
+
+                    </div>
                     {/* <Timer.Days /> days <Timer.Hours /> hours */}
+
                     <div className="time-text">
+
                       <Timer.Minutes /> minutes {" "}
 
                       <Timer.Seconds /> seconds
